a109672d6de8b2d5868ca71848f52075ce2d5e51
Rico <teh_programerer@interactivelaboratories.com>
2012-10-17 21:44:02 +0100

    |SOURCE| [Commands] Changed values in header files to enable returning the value directly from GetCommandLineParameter [Windows::Main] Implemented parsing the configuration file from the command line as a parameter, no usable configuration file yet

--- Source/Common/Commands.hpp
+++ Source/Common/Commands.hpp
@@ -2,27 +2,34 @@
 #define __UPDATER_COMMANDS_HPP__
 
 // Integer values of command-line arguments
-const int COMMAND_CONFIG_FILE		= 0x00000001;
-const int COMMAND_SITE				= 0x00000002;
-const int COMMAND_PORT				= 0x00000004;
-const int COMMAND_PROJECT			= 0x00000008;
-const int COMMAND_PLATFORM			= 0x00000010;
-const int COMMAND_BUILD_TYPE		= 0x00000020;
-const int COMMAND_DOWNLOAD_SUMMARY	= 0x00000040;
-const int COMMAND_ACTIVE_MODE		= 0x00000080;
-const int COMMAND_PORT_RANGE		= 0x00000100;
+const int COMMAND_CONFIG_FILE		= 0;
+const int COMMAND_SITE				= 1;
+const int COMMAND_PORT				= 2;
+const int COMMAND_PROJECT			= 3;
+const int COMMAND_PLATFORM			= 4;
+const int COMMAND_BUILD_TYPE		= 5;
+const int COMMAND_DOWNLOAD_SUMMARY	= 6;
+const int COMMAND_ACTIVE_MODE		= 7;
+const int COMMAND_PORT_RANGE		= 8;
+const int COMMAND_USAGE				= 9;
 const int INVALID_COMMAND			= 0xFFFFFFFF;
 
-// Returns the integer value from the command line
-int GetCommandLineParameter( const char *p_pArg, char *p_pParameter );
+const int MAX_COMMANDS	= 10;
 
-int GetSite( const char *p_pParameter, char *p_pSite );
-int GetPort( const char *p_pParameter, char *p_pPort );
-int GetProject( const char *p_pParameter, char *p_pProject );
-int GetPlatform( const char *p_pParameter, char *p_pPlatform );
-int GetBuildType( const char *p_pParameter, char *p_pBuildType );
-int GetDownloadSummary( const char *p_pParameter, char *p_pDownloadSummary );
+
+// Returns the integer value from the command line
+int GetCommandLineParameter( const char *p_pArg, char **p_ppParameter );
+
+int GetConfigFile( const char *p_pParameter, char **p_pConfigFile );
+int GetSite( const char *p_pParameter, char **p_pSite );
+int GetPort( const char *p_pParameter, char **p_pPort );
+int GetProject( const char *p_pParameter, char **p_pProject );
+int GetPlatform( const char *p_pParameter, char **p_pPlatform );
+int GetBuildType( const char *p_pParameter, char **p_pBuildType );
+int GetDownloadSummary( const char *p_pParameter, char **p_pDownloadSummary );
 int GetActiveMode( const char *p_pParameter, bool &p_ActiveMode );
-int GetPortRange( const char *p_pParameter, int *p_pRange );
+int GetPortRange( const char *p_pParameter, int **p_pRange );
+
+void DisplayUsage( );
 
 #endif
--- Source/Windows/Source/Main.cpp
+++ Source/Windows/Source/Main.cpp
@@ -4,12 +4,102 @@
 #include <FTPSite.hpp>
 #include <conio.h>
 #include <Version.h>
+#include <Commands.hpp>
+
+#define SAFE_DELETE_ARRAY( arr ) \
+	{\
+	if( arr != NULL )\
+	{\
+		delete [ ]arr;\
+		arr = NULL;\
+	}\
+	}
 
 int main( int p_Argc, char **p_ppArgv )
 {
 	std::cout << "Updater Version " << VERSION_MAJOR << "." <<
 		VERSION_MINOR << "." << VERSION_REVISION << "." <<
 		VERSION_BUILD << ( HG_LOCAL_MODIFICATIONS ? "M" : "" ) << std::endl;
+
+	std::string ConfigurationFile;
+	bool AlternateConfiguration = false;
+	bool ActiveMode = false;
+
+	if( p_Argc > 0 )
+	{
+		for( size_t i = 1; i < p_Argc; ++i )
+		{
+			char *pCommand = NULL, *pParam = NULL;
+			int Command = GetCommandLineParameter( p_ppArgv[ i ], &pCommand );
+
+			if( Command == INVALID_COMMAND )
+			{
+				SAFE_DELETE_ARRAY( pCommand );
+				DisplayUsage( );
+				return 1;
+			}
+
+			SAFE_DELETE_ARRAY( pCommand );
+
+			// Active mode and Usage commands do not accept parameters
+			if( Command == COMMAND_ACTIVE_MODE )
+			{
+				ActiveMode = true;
+				continue;
+			}
+			if( Command == COMMAND_USAGE )
+			{
+				DisplayUsage( );
+				continue;
+			}
+
+			// Process commands with a parameter
+			if( p_Argc < i+1 )
+			{
+				continue;
+			}
+
+			// Jump to the next argument
+			++i;
+			int ReturnStatus = 0;
+
+			switch( Command )
+			{
+			// Configuration file
+			case COMMAND_CONFIG_FILE:
+				{
+					ReturnStatus = GetConfigFile( p_ppArgv[ i ], &pParam );
+					if( ReturnStatus != 0 )
+					{
+						break;
+					}
+
+					ConfigurationFile.clear( );
+					ConfigurationFile.append( pParam );
+					AlternateConfiguration = true;
+
+					break;
+				}
+			default:
+				{
+					ReturnStatus = 1;
+					break;
+				}
+			}
+
+			if( ReturnStatus != 0 )
+			{
+				DisplayUsage( );
+				SAFE_DELETE_ARRAY( pCommand );
+				SAFE_DELETE_ARRAY( pParam );
+
+				return 1;
+			}
+
+			SAFE_DELETE_ARRAY( pCommand );
+			SAFE_DELETE_ARRAY( pParam );
+		}
+	}
 
 	FTPSite Site;
 
