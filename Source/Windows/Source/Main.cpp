d38397c5b58eb6dd017c2cd38a1ddc57da8481ec
Rico <teh_programerer@interactivelaboratories.com>
2012-10-20 14:33:16 +0100

    |DOCUMENTATION| [SRS] Ammended change log notes for version 0.0.0.1 [SRS] Added requirement 28
    /TODO/
    Implement a class for command-line arguments to help with program maintenance

--- Source/Windows/Source/Main.cpp
+++ Source/Windows/Source/Main.cpp
@@ -14,29 +14,34 @@ int main( int p_Argc, char **p_ppArgv )
 		VERSION_MINOR << "." << VERSION_REVISION << "." <<
 		VERSION_BUILD << ( HG_LOCAL_MODIFICATIONS ? "M" : "" ) << std::endl;
 
+	std::string ConfigurationPath;
+	ConfigurationPath.clear( );
+	ConfigFile Configuration;
 
+	if( ConfigurationPath.empty( ) )
+	{
+		Configuration.SetConfigFile( "default.cfg" );
+		if( Configuration.Parse( ) != 0 )
+		{
+			std::cout << "Could not find file test.cfg" << std::endl;
+		}
+	}
+
+	std::string Value;
+	// Print the site
+	Configuration.GetValue( "site", Value );
+	std::cout << Value.c_str( ) << std::endl;
+	/*
 	FTPSite Site;
 
 	if( Site.Initialise( ) != 0 )
 	{
 		return 1;
 	}
-	if( Site.Connect( "95.154.194.27" ) != 0 )
+	if( Site.Connect( Value.c_str( ) ) != 0 )
 	{
 		return 1;
 	}
-
-	ConfigFile TestFile( "test.cfg" );
-	if( TestFile.Parse( ) != 0 )
-	{
-		return 1;
-	}
-
-	std::string Value;
-	// Print the site
-	TestFile.GetValue( "site", Value );
-	std::cout << Value.c_str( ) << std::endl;
-
 
 	Site.ReceiveData( NULL );
 	Site.SendCommand( "USER anonymous\r\n" );
@@ -54,7 +59,7 @@ int main( int p_Argc, char **p_ppArgv )
 	Site.SendCommand( "QUIT\r\n" );
 	Site.ReceiveData( NULL );
 	Site.Disconnect( );
-
+*/
 	std::cout << "Press any key to exit" << std::endl;
 	getch( );
 
